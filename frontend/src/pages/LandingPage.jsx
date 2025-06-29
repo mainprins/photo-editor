aa29634630c17d62227887d908f016d921323fe9
Prince Bajgain <prnncebajgain@gmail.com>
2025-06-29 11:30:46 +0545

    Made Landing page horizontally responsive

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,18 +2,12 @@ import { Link } from "react-router-dom"
 
 const LandingPage = () => {
   return (
-    <div className='bg-teal-950 flex w-[100vw] h-[100vh] text-white'>
-        <div id="left" className='flex flex-col gap-[2vh] md:w-1/2 justify-center h-full px-15'>
-            <div id="top">
-                <h1 className='text-xl md:text-4xl tracking-wider font-semibold md:w-[20vw]'>Create your own version of the image.</h1>
-            </div>
-            <p className='md:w-[30vw] wrap text-md'>Edit your photos effortlessly with our powerful and easy-to-use online photo editor.</p>
-            <Link to={'/lists'}><button className='bg-amber-700 text-white py-2 md:w-[20%] rounded-md hover:bg-teal-600 cursor-pointer transition-colors duration-700'>Your Projects</button></Link>
-        </div>
-        <div id="right" className='hidden md:flex justify-center items-center md:w-1/2 h-full'>
-           <figure className=" bg-linear-to-br from-teal-500 to-amber-500 w-[30vw] h-[30vw] rounded-t-full relative">
+    <div className='bg-teal-950 flex flex-col md:flex-row w-[100vw] h-[100vh] text-white'>
+        
+        <div id="right" className='flex justify-center items-center md:w-1/2 pt-12 xl:pt-0 h-[60vh] md:h-full'>
+           <figure className=" bg-linear-to-br from-teal-500 to-amber-500 w-[60vw] h-[40vh] xl:h-[60vh] xl:w-[30vw] sm:h-[40vh] sm:w-[40vw] rounded-t-full relative">
              <img src="/personPhotifier.png" alt="PersonLiking" className="object-contain w-full h-full scale-x-[-1]" />
-             <div className="backdrop-blur-3xl rounded-md absolute top-15 -left-10 w-[10vw] p-4 flex gap-4 flex-col text-black h-[25vh]">
+             <div className="hidden xl:flex backdrop-blur-3xl rounded-md absolute top-15 -left-16 w-[15vw] p-4 gap-4 flex-col text-white h-[25vh]">
                 <h4>Adjustments</h4>
                 <div className="flex w-[70%] justify-between">
                       <span className="text-xs">Brightness</span>
@@ -32,10 +26,17 @@ const LandingPage = () => {
                     <div className="rounded-full absolute ring-1 ring-gray-700 -top-2 right-10 w-[1vw] h-[2vh]"></div>
                 </div>
              </div>
-             <div className="bg-teal-500 w-[4vw] h-[8vh] rounded-full absolute bottom-10 -left-10"></div>
-             <div className="bg-amber-300 w-[4vw] h-[8vh] rounded-full absolute bottom-80 -right-3"></div>
+             <div className="hidden md:block bg-teal-500 w-[4vw] xl:w-[3vw] h-[4vh] rounded-full absolute bottom-15 -left-6 "></div>
+             <div className="hidden md:block bg-amber-300 w-[4vw] xl:w-[3vw]  h-[4vh] rounded-full absolute bottom-70 -right-4 xl:bottom-50"></div>
            </figure>
            
+        </div>
+        <div id="left" className='flex flex-col gap-[2vh] md:w-1/2 justify-center px-15'>
+            <div id="top">
+                <h1 className='text-xl md:text-4xl tracking-wider font-semibold md:w-[20vw]'>Create your own version of the image.</h1>
+            </div>
+            <p className='md:w-[30vw] wrap text-md'>Edit your photos effortlessly with our powerful and easy-to-use online photo editor.</p>
+            <Link to={'/lists'}><button className='bg-amber-700 text-white py-2 px-3 md:px-6 rounded-md hover:bg-teal-600 cursor-pointer transition-colors duration-700'>Your Projects</button></Link>
         </div>
     </div>
   )
